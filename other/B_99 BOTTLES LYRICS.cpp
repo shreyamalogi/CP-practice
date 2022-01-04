0d5bf50f8e62dc5132a314294faebb38cd5a49d6
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-01-05 00:40:27 +0530

    random

---
+++ other/B_99 BOTTLES LYRICS.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+    {
+    int count = 99;
+    while ( count > 0 )
+        {
+        cout << count << " bottle of beer on the wall," << endl;
+        cout << count << " bottle of beer." << endl;
+        cout << "Take one down, pass it around," << endl;
+        cout << count-- << " bottle of beer on the wall." << endl;
+        cout << endl;
+        }
+        cout<<`
+}
+
+	
+	
+	
+	
+	
+
